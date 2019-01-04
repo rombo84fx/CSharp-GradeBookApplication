9cf179458c6f0fe1f4c8a6b32e838c2c036acbcb
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2019-01-04 12:40:51 -0600

    Finish Creating New GradeBookTypes

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard = 0,
+        Ranked = 1,
+        ESNU = 2,
+        OneToFour = 3,
+        SixPoint = 4
+    }
+}
