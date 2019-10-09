4690998af26abdcd27674a34ecd84cda3c8278cd
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2019-10-08 21:59:29 -0500

    Finish Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,5 +38,21 @@ namespace GradeBook.GradeBooks
             }
             return numberOfGrades <= threshold * 4 ? 'D' : 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
